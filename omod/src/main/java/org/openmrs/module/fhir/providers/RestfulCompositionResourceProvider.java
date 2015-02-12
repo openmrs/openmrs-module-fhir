b60a5734a3898a7e7d5186048e71f8ae61c54a55
harsha89 <harsz89@gmail.com>
2015-02-13 04:26:27 +0530

    adding files

---
+++ omod/src/main/java/org/openmrs/module/fhir/providers/RestfulCompositionResourceProvider.java
@@ -0,0 +1,49 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.fhir.providers;
+
+import ca.uhn.fhir.model.api.IResource;
+import ca.uhn.fhir.model.dstu.resource.Composition;
+import ca.uhn.fhir.rest.annotation.RequiredParam;
+import ca.uhn.fhir.rest.annotation.Search;
+import ca.uhn.fhir.rest.param.ReferenceParam;
+import ca.uhn.fhir.rest.server.IResourceProvider;
+import org.openmrs.module.fhir.resources.FHIRCompositionResource;
+
+import java.util.List;
+
+public class RestfulCompositionResourceProvider implements IResourceProvider {
+
+	private FHIRCompositionResource compositionResource;
+
+	public RestfulCompositionResourceProvider() {
+		compositionResource = new FHIRCompositionResource();
+	}
+
+	@Override
+	public Class<? extends IResource> getResourceType() {
+		return Composition.class;
+	}
+
+	/**
+	 * Search locations by unique id
+	 *
+	 * @param patient object containing the requested id
+	 */
+	@Search()
+	public List<Composition> searchEncountersByUniqueId(@RequiredParam(name = Composition.SP_SUBJECT) ReferenceParam
+			                                                    patient) {
+		return compositionResource.searchEncounterCompostionsByPatient(patient);
+	}
+}
