48ba1039d9586f5595e6da8206b46a845bbb52ac
harsha89 <harsz89@gmail.com>
2015-05-07 00:31:54 +0530

    adding rest client missing class

---
+++ omod/src/main/java/org/openmrs/module/fhir/web/controller/FHIRClientController.java
@@ -0,0 +1,36 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.fhir.web.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * The main controller.
+ */
+@Controller
+public class FHIRClientController {
+	
+	protected final static Log log = LogFactory.getLog(FHIRClientController.class);
+
+	@RequestMapping(value = "/module/fhir/client", method = RequestMethod.GET)
+	public void manage(ModelMap model) {
+		model.addAttribute("user", Context.getAuthenticatedUser());
+	}
+}
