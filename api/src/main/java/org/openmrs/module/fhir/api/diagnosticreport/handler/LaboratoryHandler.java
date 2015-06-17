ed1cd0728e71d9a991c79ae4e6e98af23af6c7b6
Milan Karunarathne <mhkarunarathne@gmail.com>
2015-06-18 00:32:56 +0530

    Created an Encounter successfully with minimum requirements

--- api/src/main/java/org/openmrs/module/fhir/api/diagnosticreport/handler/LaboratoryHandler.java
+++ api/src/main/java/org/openmrs/module/fhir/api/diagnosticreport/handler/LaboratoryHandler.java
@@ -1,8 +1,10 @@
 package org.openmrs.module.fhir.api.diagnosticreport.handler;
 
+import ca.uhn.fhir.model.dstu2.composite.CodingDt;
 import ca.uhn.fhir.model.dstu2.resource.DiagnosticReport;
 import ca.uhn.fhir.model.dstu2.resource.Patient;
 import ca.uhn.fhir.model.dstu2.resource.Practitioner;
+import ca.uhn.fhir.model.primitive.IdDt;
 import org.openmrs.Encounter;
 import org.openmrs.EncounterRole;
 import org.openmrs.Person;
@@ -16,6 +18,8 @@ import org.openmrs.module.fhir.api.diagnosticreport.DiagnosticReportHandler;
 import org.openmrs.module.fhir.api.impl.PatientServiceImpl;
 import org.openmrs.module.fhir.api.util.FHIRPatientUtil;
 import org.openmrs.module.fhir.api.util.FHIRPractitionerUtil;
+
+import java.util.List;
 
 public class LaboratoryHandler extends AbstractHandler implements DiagnosticReportHandler {
 
@@ -41,13 +45,12 @@ public class LaboratoryHandler extends AbstractHandler implements DiagnosticRepo
 
 	@Override
 	public DiagnosticReport saveFHIRDiagnosticReport(DiagnosticReport diagnosticReport) {
-		log.info("Laboratory Handler : diSave FHIR Diagnostic Report");
+		log.info("Laboratory Handler : Save FHIR Diagnostic Report");
 		Encounter omrsDiagnosticReport = new Encounter();
 
 		//Set ID if available
 		if (diagnosticReport.getId() != null) {
 			omrsDiagnosticReport.setUuid(diagnosticReport.getId().getIdPart());
-			log.info("Set UUid: " + omrsDiagnosticReport.getUuid());
 		}
 
 		// Set `Name` as a Obs
@@ -59,42 +62,48 @@ public class LaboratoryHandler extends AbstractHandler implements DiagnosticRepo
 		// Set `Subject` as Encounter Patient
 		if (diagnosticReport.getSubject().getReference().isLocal()) {
 			Patient patient = (Patient) diagnosticReport.getSubject().getResource();
-			// org.openmrs.Patient omrsParient = FHIRPatientUtil.generatePatient(patient);
+			//TODO: org.openmrs.Patient omrsParient = FHIRPatientUtil.generateOpenMRSPatient(patient);
 			org.openmrs.Patient omrsParient = new org.openmrs.Patient();
 			omrsDiagnosticReport.setPatient(omrsParient);
 		} else {
 			// Get Id of the Patient
-			String id = diagnosticReport.getSubject().getReference().getIdPart();
-			log.info("Patient id : " + id);
-
+			String patientID = diagnosticReport.getSubject().getReference().getIdPart();
 			// Assume that Patient is stored in the OpenMRS database
 			PatientService fhirPatientService = Context.getService(PatientService.class);
-			Patient patient = fhirPatientService.getPatient("123");
+			Patient patient = fhirPatientService.getPatient(patientID);
 			// org.openmrs.Patient omrsPatient = FHIRPatientUtil.generatePatient(patient);
-			org.openmrs.Patient omrsPatient = new org.openmrs.Patient();
+			org.openmrs.Patient omrsPatient = Context.getPatientService().getPatientByUuid(patientID);
 			omrsDiagnosticReport.setPatient(omrsPatient);
 		}
 
-		// Set `Performer` as Encounter Provider
+		// Set `Performer`(Practitioner) as Encounter Provider
 		if (diagnosticReport.getSubject().getReference().isLocal()) {
 			Practitioner practitioner = (Practitioner) diagnosticReport.getPerformer().getResource();
-			// org.openmrs.Provider omrsProvider = FHIRPractitionerUtil.generatePractitioner();
+			//TODO: org.openmrs.Provider omrsProvider = FHIRPractitionerUtil.generatePractitioner();
 			Provider omrsProvider = new Provider();
 			omrsDiagnosticReport.setProvider(new EncounterRole(), omrsProvider);
 		} else {
 			// Get Id of the Performer
-			String id = diagnosticReport.getPerformer().getReference().getIdPart();
-			log.info("Performer id : " + id);
-
+			String practitionerID = diagnosticReport.getPerformer().getReference().getIdPart();
 			// Assume that Performer is stored in the OpenMRS database
 			PractitionerService fhirPractitionerService = Context.getService(PractitionerService.class);
-			Practitioner practitioner = fhirPractitionerService.getPractitioner("123");
-			// org.openmrs.Provider omrsProvider = FHIRPractitionerUtil.generatePractitioner();
-			Provider omrsProvider = new Provider();
-			omrsDiagnosticReport.setProvider(new EncounterRole(), omrsProvider);
+			Practitioner practitioner = fhirPractitionerService.getPractitioner(practitionerID);
+			//TODO: org.openmrs.Provider omrsProvider = FHIRPractitionerUtil.generateOpenMRSPractitioner();
+			Provider omrsProvider = Context.getProviderService().getProviderByUuid(practitionerID);
+			//TODO: Get EncounterRole from DiagnosticReport (remove hard coded value)
+			String encounterRoleID = "73bbb069-9781-4afc-a9d1-54b6b2270e03";
+			EncounterRole encounterRole = Context.getEncounterService().getEncounterRoleByUuid(encounterRoleID);
+			omrsDiagnosticReport.setProvider(encounterRole, omrsProvider);
 		}
 
 		// Set `ServiceCategory` as EncounterType
+		List<CodingDt> codingList = diagnosticReport.getServiceCategory().getCoding();
+		String encounterType = "DEFAULT"; // If serviceCategory is not present in the DiagnosticReport, then use "DEFAULT"
+		if (!codingList.isEmpty()) {
+			encounterType = codingList.get(0).getCode();
+		}
+		omrsDiagnosticReport.setEncounterType(Context.getEncounterService().getEncounterType(encounterType));
+
 		// Set `Diagnosis[x]->DateTime` as valueDateTime in an Obs
 		// Set `Diagnosis[x]->Period` as valueDateTime in an Obs
 
@@ -104,10 +113,7 @@ public class LaboratoryHandler extends AbstractHandler implements DiagnosticRepo
 		// Create resource in OpenMRS Database
 		EncounterService encounterService = Context.getEncounterService();
 		Encounter omrsEncounter = encounterService.saveEncounter(omrsDiagnosticReport);
-
-		// Testing purpose
-		log.info("Created OpenMRS encounter : " + omrsEncounter.getUuid());
-		encounterService.purgeEncounter(omrsEncounter, true);
+		diagnosticReport.setId(new IdDt("DiagnosticReport", omrsEncounter.getUuid()));
 		return diagnosticReport;
 	}
 
