47c06d50270ecad30e708d749f50a5ab22ed546f
areklalo <alalo@soldevelo.com>
2019-02-18 13:38:40 +0100

    SYNCT-180: Added missing logic used to synchronize the Obs

--- api/src/main/java/org/openmrs/module/fhir/api/util/FHIRObsUtil.java
+++ api/src/main/java/org/openmrs/module/fhir/api/util/FHIRObsUtil.java
@@ -13,12 +13,15 @@
  */
 package org.openmrs.module.fhir.api.util;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hl7.fhir.dstu3.model.Attachment;
 import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.fhir.dstu3.model.Coding;
 import org.hl7.fhir.dstu3.model.DateTimeType;
+import org.hl7.fhir.dstu3.model.Extension;
 import org.hl7.fhir.dstu3.model.Observation;
 import org.hl7.fhir.dstu3.model.Period;
 import org.hl7.fhir.dstu3.model.Quantity;
@@ -30,9 +33,10 @@ import org.openmrs.ConceptMap;
 import org.openmrs.ConceptNumeric;
 import org.openmrs.Encounter;
 import org.openmrs.EncounterProvider;
+import org.openmrs.Location;
 import org.openmrs.Obs;
 import org.openmrs.api.context.Context;
-import org.openmrs.module.fhir.api.helper.ObsHelper;
+import org.openmrs.module.fhir.api.constants.ExtensionURL;
 import org.openmrs.obs.ComplexData;
 
 import java.math.BigDecimal;
@@ -61,13 +65,13 @@ public class FHIRObsUtil {
 		DateTimeType type = new DateTimeType();
 		type.setValue(obs.getObsDatetime());
 		observation.setEffective(type);
-
 		//Set fhir observation comment
 		observation.setComment(obs.getComment());
 		observation.setSubject(FHIRUtils.buildPatientOrPersonResourceReference(obs.getPerson()));
 		//Set fhir performers from openmrs providers
 		List<Reference> performers = new ArrayList<>();
 		if (obs.getEncounter() != null) {
+			observation.setContext(getFHIREncounterReference(obs.getEncounter()));
 			for (EncounterProvider provider : obs.getEncounter().getEncounterProviders()) {
 				Reference providerReference = new Reference();
 				StringBuilder providerNameDisplay = new StringBuilder();
@@ -156,20 +160,9 @@ public class FHIRObsUtil {
 			observation.setValue(datetime);
 
 		} else if (FHIRConstants.CWE_HL7_ABBREVATION.equalsIgnoreCase(obs.getConcept().getDatatype().getHl7Abbreviation())) {
-			if (obs.getValueCoded() != null) {
-				Collection<ConceptMap> valueMappings = obs.getValueCoded().getConceptMappings();
-				List<Coding> values = new ArrayList<>();
-				//Set codings from openmrs concept mappings
-				for (ConceptMap map : valueMappings) {
-					if (map.getConceptReferenceTerm() != null) {
-						values.add(FHIRUtils.createCoding(map));
-					}
-				}
-				//Set openmrs concept
-				values.add(FHIRUtils.getCodingDtByOpenMRSConcept(obs.getValueCoded()));
-				CodeableConcept codeableConceptDt = new CodeableConcept();
-				codeableConceptDt.setCoding(values);
-				observation.setValue(codeableConceptDt);
+			Concept concept = obs.getValueCoded();
+			if (concept != null) {
+				observation.setValue(FHIRUtils.createCodeableConcept(concept));
 			}
 		} else if (FHIRConstants.ED_HL7_ABBREVATION.equalsIgnoreCase(obs.getConcept().getDatatype().getHl7Abbreviation())) {
 			Attachment attachmentDt = new Attachment();
@@ -199,7 +192,7 @@ public class FHIRObsUtil {
 				related.setType(Observation.ObservationRelationshipType.HASMEMBER);
 				resourceReferenceDt = new Reference();
 				resourceReferenceDt.setDisplay(ob.getConcept().getName().getName());
-				String obsUri = FHIRConstants.OBSERVATION + "/" + obs.getUuid();
+				String obsUri = FHIRConstants.OBSERVATION + "/" + ob.getUuid();
 				resourceReferenceDt.setReference(obsUri);
 				related.setTarget(resourceReferenceDt);
 				relatedObs.add(related);
@@ -221,6 +214,17 @@ public class FHIRObsUtil {
 			relatedObs.add(related);
 		}
 		observation.setRelated(relatedObs);
+		if (obs.getObsGroup() != null) {
+			Observation.ObservationRelatedComponent related = new Observation.ObservationRelatedComponent();
+			related.setType(Observation.ObservationRelationshipType.DERIVEDFROM);
+			Reference resourceReferenceDt = new Reference();
+			resourceReferenceDt.setDisplay(obs.getObsGroup().getConcept().getName().getName());
+			String obsUri = FHIRConstants.OBSERVATION + "/" + obs.getObsGroup().getUuid();
+			resourceReferenceDt.setReference(obsUri);
+			related.setTarget(resourceReferenceDt);
+			observation.addRelated(related);
+		}
+		observation.addExtension(buildLocationExtension(obs.getLocation()));
 		return observation;
 	}
 
@@ -242,6 +246,10 @@ public class FHIRObsUtil {
 		Obs obs = new Obs();
 
 		BaseOpenMRSDataUtil.readBaseExtensionFields(obs, observation);
+
+		if (StringUtils.isNotBlank(observation.getId())) {
+			obs.setUuid(FHIRUtils.extractUuid(observation.getId()));
+		}
 
 		obs.setComment(observation.getComment());
 		if (observation.getSubject() != null) {
@@ -337,16 +345,72 @@ public class FHIRObsUtil {
 					ComplexData data = new ComplexData("images.JPEG", byteStream);
 					obs.setValueComplex(byteStream.toString());
 					obs.setComplexData(data);
+				} else if (FHIRConstants.CWE_HL7_ABBREVATION.equalsIgnoreCase(concept.getDatatype().getHl7Abbreviation())) {
+					CodeableConcept codeableConcept = (CodeableConcept) observation.getValue();
+					obs.setValueCoded(FHIRUtils.getConceptByCodeableConcept(codeableConcept));
 				}
 			}
 		}
+
+		if (hasGroupMembers(observation) && StringUtils.isBlank(obs.getValueText())) {
+			//TODO: Used to correctly save obs when the parent obs of group is save as first, see SpecificObsValidator.validate
+			obs.setValueText(FHIRConstants.OBS_GROUP_MEMBER_TEXT_VALUE);
+		}
+		setObsGroup(observation, obs);
 
 		CodeableConcept interpretation = observation.getInterpretation();
 		ContextUtil.getObsHelper().setInterpretation(obs, interpretation);
 		Observation.ObservationStatus status = observation.getStatus();
 		ContextUtil.getObsHelper().setStatus(obs, status);
+		obs.setLocation(buildLocationBasedOnExtension(observation));
 
 		return obs;
+	}
+
+	private static Extension buildLocationExtension(Location location) {
+		Extension result = null;
+		if (location != null) {
+			result = ExtensionsUtil.createLocationUuidExtension(location);
+		}
+		return result;
+	}
+
+	private static Location buildLocationBasedOnExtension(Observation observation) {
+		String locationUuid = getFormUuidFromExtension(observation);
+		if (StringUtils.isNotBlank(locationUuid)) {
+			return Context.getLocationService().getLocationByUuid(locationUuid);
+		}
+		return null;
+	}
+
+	private static String getFormUuidFromExtension(Observation observation) {
+		List<Extension> extensions = observation.getExtensionsByUrl(ExtensionURL.LOCATION_UUID_URL);
+		if (!CollectionUtils.isEmpty(extensions)) {
+			return ExtensionsUtil.getStringFromExtension(extensions.get(FHIRConstants.FIRST));
+		}
+		return null;
+	}
+
+	private static void setObsGroup(Observation observation, Obs obs) {
+		for (Observation.ObservationRelatedComponent component : observation.getRelated()) {
+			if (component.getType().equals(Observation.ObservationRelationshipType.DERIVEDFROM)) {
+				String parentUuid = FHIRUtils.getObjectUuidByReference(component.getTarget());
+				Obs parent = Context.getObsService().getObsByUuid(parentUuid);
+				obs.setObsGroup(parent);
+				break;
+			}
+		}
+	}
+
+	public static boolean hasGroupMembers(Observation observation) {
+		boolean hasMembers = false;
+		for (Observation.ObservationRelatedComponent component : observation.getRelated()) {
+			if (component.getType().equals(Observation.ObservationRelationshipType.HASMEMBER)) {
+				hasMembers = true;
+				break;
+			}
+		}
+		return hasMembers;
 	}
 
 	public static Obs copyObsAttributes(Obs requestObs, Obs retrievedObs, List<String> errors) {
@@ -367,6 +431,8 @@ public class FHIRObsUtil {
 				}
 			} else if (FHIRConstants.BIT_HL7_ABBREVATION.equalsIgnoreCase(concept.getDatatype().getHl7Abbreviation())) {
 				retrievedObs.setValueCoded(requestObs.getValueCoded());
+			} else if (FHIRConstants.CWE_HL7_ABBREVATION.equalsIgnoreCase(concept.getDatatype().getHl7Abbreviation())) {
+				retrievedObs.setValueCoded(requestObs.getValueCoded());
 			} else if (FHIRConstants.TS_HL7_ABBREVATION.equalsIgnoreCase(concept.getDatatype().getHl7Abbreviation())) {
 				retrievedObs.setValueDatetime(requestObs.getValueDatetime());
 			} else if (FHIRConstants.DT_HL7_ABBREVATION.equalsIgnoreCase(concept.getDatatype().getHl7Abbreviation())) {
@@ -376,6 +442,8 @@ public class FHIRObsUtil {
 			}
 		}
 		retrievedObs.setComment(requestObs.getComment());
+		retrievedObs.setEncounter(requestObs.getEncounter());
+		retrievedObs.setLocation(requestObs.getLocation());
 		return retrievedObs;
 	}
 
